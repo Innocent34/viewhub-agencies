3400544c453604ab9d22108406058b217a14e0b8
Innocent34 <156189301+Innocent34@users.noreply.github.com>
2026-09-18 11:20:24 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+{
+  "name": "viewhub-agencies",
+  "version": "1.0.0",
+  "description": "ViewHub Agencies website",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^5.1.0",
+    "axios": "^1.12.2"
+  }
+}
